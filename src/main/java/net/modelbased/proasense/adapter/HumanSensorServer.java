1bc36b0af4a963fbc32a7c0dd66105ae49606b77
Brian Elvesæter <brian.elvesater@sintef.no>
2015-05-21 16:04:51 +0200

    First version of adapter.

---
+++ src/main/java/net/modelbased/proasense/adapter/HumanSensorServer.java
@@ -0,0 +1,144 @@
+/**
+ * Copyright (C) 2014-2015 SINTEF
+ *
+ *     Brian Elvesæter <brian.elvesater@sintef.no>
+ *     Shahzad Karamat <shazad.karamat@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.modelbased.proasense.adapter;
+
+import eu.proasense.internal.ComplexValue;
+import eu.proasense.internal.SimpleEvent;
+import eu.proasense.internal.VariableType;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.thrift.TException;
+import org.apache.thrift.TSerializer;
+import org.apache.thrift.protocol.TBinaryProtocol;
+
+import org.joda.time.DateTime;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/humansensorserver")
+public class HumanSensorServer extends ProaSenseBaseAdapter {
+
+	ArrayList al = new ArrayList();
+	
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response HumanSensorServer(String text){
+		String zooKeeper = "89.216.116.44:2181";
+		String topic = "proasense.datainfrastructure.mhwirth.all";
+
+		// Create the Kakfa producer
+		producer = createProducer(zooKeeper);
+
+		try {
+			// Convert to simple event
+			SimpleEvent event = convertToSimpleEvent(text);
+			System.out.println("SimpleEvent: " + event.toString());
+
+			// Serialize message
+			TSerializer serializer = new TSerializer(new TBinaryProtocol.Factory());
+			byte[] bytes = serializer.serialize(event);
+
+			// Publish message
+			ProducerRecord<String, byte[]> message = new ProducerRecord<String, byte[]>(topic, bytes);
+			producer.send(message);
+		}
+		catch (TException ignore) {
+
+		}
+		finally {
+			producer.close();
+		}
+
+		return Response.status(201).entity(text).build();
+	}
+	
+
+	private SimpleEvent convertToSimpleEvent(String text){
+		String[] splitText = text.split("-");
+		String[] sensorValues = splitText[0].split(",");
+		String[] valueTypes = splitText[1].split(",");
+		String sensorId = valueTypes[0];
+		
+	//	System.out.println(sensorId);
+	//	System.out.println(sensorValues[1]);
+	//	System.out.println(valueTypes[0]);
+
+		// Process event properties
+		Map<String, ComplexValue> properties = new HashMap<String, ComplexValue>();
+
+		String processedValues = null;
+		int n = 1;
+		for (int i = 0; i < sensorValues.length; i++) {
+			i++;
+
+//			processedValues = sensorValues[i++]+",";
+//			processedValues += sensorValues[i]+",";
+//          processedValues += valueTypes[n++];
+
+            String propertyKey = sensorValues[i++];
+            String propertyValue = sensorValues[i];
+            String propertyType = valueTypes[n++];
+
+            // Define complex value
+			ComplexValue value = new ComplexValue();
+			value.setValue(propertyValue);
+
+			// if statement to check for proper value type
+            if (propertyType.matches("LONG")) {
+                value.setType(VariableType.LONG);
+            }
+            if (propertyType.matches("STRING")) {
+                value.setType(VariableType.STRING);
+            }
+            if (propertyType.matches("DOUBLE")) {
+                value.setType(VariableType.DOUBLE);
+            }
+            if (propertyType.matches("BLOB")) {
+                value.setType(VariableType.BLOB);
+            }
+
+			properties.put(propertyKey, value);
+		}
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		Date date = new Date();
+		String sensorTime = dateFormat.format(date);
+        long timestamp = new DateTime(sensorTime).getMillis();
+		
+		SimpleEvent event = new SimpleEvent();
+		event.setTimestamp(timestamp);
+        event.setSensorId(sensorId);
+        event.setEventProperties(properties);
+
+		return event;
+	}
+}
