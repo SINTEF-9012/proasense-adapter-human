40e74eeccda500becca27973ff42ee3d2d5d4cc2
Brian Elvesæter <brian.elvesater@sintef.no>
2015-08-12 15:09:51 +0200

    Refactored project into a multi-module Maven project.

--- human-base/src/main/java/net/modelbased/proasense/adapter/HumanSensorServer.java
+++ human-base/src/main/java/net/modelbased/proasense/adapter/HumanSensorServer.java
@@ -26,7 +26,10 @@ import org.apache.thrift.TException;
 import org.apache.thrift.TSerializer;
 import org.apache.thrift.protocol.TBinaryProtocol;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.text.SimpleDateFormat;
