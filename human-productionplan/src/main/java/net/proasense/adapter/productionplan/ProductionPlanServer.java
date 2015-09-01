22bc882742dbc5c81b7b4e2c449c655924337fd7
shakar70 <shakar70@gmail.com>
2015-09-01 18:20:27 +0200

    working form.

--- human-productionplan/src/main/java/net/proasense/adapter/productionplan/ProductionPlanServer.java
+++ human-productionplan/src/main/java/net/proasense/adapter/productionplan/ProductionPlanServer.java
@@ -3,6 +3,7 @@ package net.proasense.adapter.productionplan;
 import eu.proasense.internal.ComplexValue;
 import eu.proasense.internal.SimpleEvent;
 import eu.proasense.internal.VariableType;
+import net.modelbased.proasense.adapter.base.AbstractBaseAdapter;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -21,48 +22,70 @@ import java.util.Map;
  */
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/productionPlan")
-public class ProductionPlanServer {
+public class ProductionPlanServer extends AbstractBaseAdapter {
     // The Java method will process HTTP GET requests
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public void convertToSimpleEvents(String text){
-
-     /*   String allValues[] = text.split(",");
-
-        String sensorId = allValues[0];
-        long timestamp = System.currentTimeMillis();
-        int shiftid_value = 4;
-        String productId = allValues[1];*/
+    public void convertToSimpleEvents(String text) {
 
         System.out.println(text);
 
-/*        ComplexValue complexValue = new ComplexValue();
+        String time_value[] = {"6:00", "14:00", "22:00", "6:00"};
+        String allValues[] = text.split(",");
+
+        String machineId = allValues[0];
+        long timestamp = System.currentTimeMillis();
+        String shiftid = allValues[1];
+        String productId = allValues[2];
+        String shiftStartTime = time_value[Integer.parseInt(shiftid)-1];
+        String shiftEndTime = time_value[Integer.parseInt(shiftid)];
+        if (allValues.length == 3) {
+            //send start og slutt til en metode.
+            makeAndPublishEvents(machineId, timestamp, shiftid, productId, shiftStartTime, shiftEndTime);
+        } else if (allValues.length == 5) {
+            shiftEndTime = allValues[4];
+            makeAndPublishEvents(machineId, timestamp, shiftid, productId, shiftStartTime, shiftEndTime);
+            //send til metode med atsrt og end tiem.
+            shiftStartTime = shiftEndTime;
+            shiftEndTime = time_value[Integer.parseInt(shiftid)];
+            productId = allValues[3];
+            makeAndPublishEvents(machineId, timestamp, shiftid, productId, shiftStartTime, shiftEndTime);
+        }
+    }
+
+    public void makeAndPublishEvents(String machineId, long timestamp, String shiftid,
+                                     String productId, String prodStartTime, String prodEndTime){
+
+         ComplexValue complexValue = new ComplexValue();
          Map<String, ComplexValue> eventProperties = new HashMap<String, ComplexValue>();
-        SimpleEvent simpleEvent = new SimpleEvent();
+         SimpleEvent simpleEvent = new SimpleEvent();
 
-        sensorId = "materialchange"; //
-        long timeStamp = System.currentTimeMillis(); //
+        simpleEvent.setSensorId(machineId);
+        simpleEvent.setTimestamp(timestamp);
 
-        String machineId = allValues[0]; //
-        String materialId =allValues[1]; //
-
-
-        simpleEvent.sensorId = sensorId;
-        simpleEvent.timestamp = timeStamp;
-
-        complexValue.setValue(machineId);
-        complexValue.setType(VariableType.STRING);
-        simpleEvent.putToEventProperties("machineid", complexValue);
+        complexValue.setValue(shiftid);
+        complexValue.setType(VariableType.LONG);
+        simpleEvent.putToEventProperties("shifId", complexValue);
 
         complexValue = new ComplexValue();
-        complexValue.setValue(materialId);
+        complexValue.setValue(productId);
         complexValue.setType(VariableType.STRING);
-        simpleEvent.putToEventProperties("materialId", complexValue);
+        simpleEvent.putToEventProperties("productId", complexValue);
 
+        complexValue = new ComplexValue();
+        complexValue.setValue(prodStartTime);
+        complexValue.setType(VariableType.LONG);
+        simpleEvent.putToEventProperties("productStartTime", complexValue);
 
-       this.outputPort.publishSimpleEvent(simpleEvent);*/
+        complexValue = new ComplexValue();
+        complexValue.setValue(prodEndTime);
+        complexValue.setType(VariableType.LONG);
+        simpleEvent.putToEventProperties("productEndTime", complexValue);
+
+            System.out.println(simpleEvent.toString());
+        this.outputPort.publishSimpleEvent(simpleEvent);
 
     }
 }
